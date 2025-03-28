5ea59a477bbdff540f4355fbc556d2f6a758c111
Hail <jyealc@gmail.com>
2025-03-28 17:10:08 +0800

    View projects

--- main.py
+++ main.py
@@ -6,18 +6,50 @@ class User:
         self.maritalStatus = maritalStatus
         self.password = password
 
+    def menu(self):
+        ...
 class Applicant(User):
     def __init__(self, name, nric, age, maritalStatus, password):
         super().__init__(name, nric, age, maritalStatus, password)
 
+    def menu(self):
+        return [
+            "View Projects",
+            "Apply for Projects",
+            "View Application Status",
+            "Logout",
+        ]
+    
+    def getAvailableProjects(self, projectController):
+        projects = []
+        for i, project in enumerate(projectController.projects):
+            if project.visibility:
+                projects.append(f"ID: {i}\nName: {project.projectName}\nNeighborhood: {project.neighborhood}\n{project.type1}: {project.numUnits1} ({project.price1})\n{project.type2}: {project.numUnits2} ({project.price2})\nApplication Date: {project.openingDate}-{project.closingDate}")
+        return projects
+                
 class HDBOfficer(Applicant):
     def __init__(self, name, nric, age, maritalStatus, password):
         super().__init__(name, nric, age, maritalStatus, password)
 
+    def menu(self):
+        return [
+            "View Projects",
+            "Apply for Projects",
+            "View Application Status",
+            "Logout",
+        ]
+
 class HDBManager(User):
     def __init__(self, name, nric, age, maritalStatus, password):
         super().__init__(name, nric, age, maritalStatus, password)
-
+    
+    def menu(self):
+        return [
+            "View Projects",
+            "Apply for Projects",
+            "View Application Status",
+            "Logout",
+        ]
 class Project:
     def __init__(self, projectName, neighborhood, type1, numUnits1, price1, type2, numUnits2, price2, openingDate, closingDate, manager, officerSlot, officer):
         self.projectName = projectName
@@ -33,6 +65,7 @@ class Project:
         self.manager = manager
         self.officerSlot = officerSlot
         self.officer = officer
+        self.visibility = True
 
 class UsersController:
     def __init__(self, applicantCsv, hdbOfficerCsv, hdbManagerCsv):
@@ -77,24 +110,28 @@ class ProjectsController:
     def __init__(self, projectCsv):
         self.projects = []
 
-        for line in open(projectCsv, 'r').readlines():
-            line = line.split()
-            row = []
-            current = ""
-            inside_quotes = False
+        result = []
+        for line in open(projectCsv, 'r').readlines()[1:]:
+            line = line.strip()
+            parts = []
+            temp = ''
+            in_quotes = False
             for char in line:
-                if char == '"' and not inside_quotes:
-                    inside_quotes = True
-                elif char == '"' and inside_quotes:
-                    inside_quotes = False
-                elif char == ',' and not inside_quotes:
-                    row.append(current)
-                    current = ""
+                if char == '"':
+                    in_quotes = not in_quotes
+                elif char == ',' and not in_quotes:
+                    parts.append(temp)
+                    temp = ''
                 else:
-                    current += char
-            row.append(current)
-            self.projects.append(Project(current[0], current[1], current[2], current[3], current[4], current[5], current[6], current[7], current[8], current[9], current[10], current[11], current[12]))
-        
+                    temp += char
+
+            parts.append(temp)
+            parts[-1] = parts[-1].split(',')
+            result.append(parts)
+
+        for parts in result:
+            self.projects.append(Project(parts[0], parts[1], parts[2], int(parts[3]), int(parts[4]), parts[5], int(parts[6]), int(parts[7]), parts[8], parts[9], parts[10], parts[11], parts[12]))
+
 
 if __name__ == "__main__":
     usersController = UsersController('ApplicantList.csv', 'OfficerList.csv', 'ManagerList.csv')
@@ -111,4 +148,21 @@ if __name__ == "__main__":
 
         print(f"Welcome, {currentUser.name} ({UsersController.getUserType(currentUser)})")
 
-        break
+        while currentUser:
+            for i, option in enumerate(currentUser.menu()):
+                print(f"{i + 1}. {option}")
+            choice = input("Enter choice: ")
+            if choice == "1":
+                projects = currentUser.getAvailableProjects(projectsController)
+                for project in projects:
+                    print(project)
+                    print()
+            elif choice == "2":
+                print("Apply for Projects")
+            elif choice == "3":
+                print("View Application Status")
+            elif choice == "4":
+                print("Logout")
+                currentUser = None
+            else:
+                print("Invalid choice")
